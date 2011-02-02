c7aab651d4b2a2c59946cd2dcdf4be6f0aa2e1f0
Gmanshetty <gmanshetty@ymail.com>
2011-02-02 01:50:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barmoney::Application.config.secret_token = 'a265d4c55569c1967aaaae84d36d97a5de866d4a5cbacbe4dd4577f0c1ff661216337f2ef2898e415a7724380a0198f4b380e30b87e3e0408af2d28ff6d17dbb'
